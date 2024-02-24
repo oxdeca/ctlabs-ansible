92ed2e055e7a43c361541453f5b3564c8d3cf8c4
acedxo <acedxo@oxdeca.com>
2024-02-24 00:02:42 -0500

    improved role ctlabs_ca

--- roles/ctlabs_ca/files/create_server_cert.sh
+++ roles/ctlabs_ca/files/create_server_cert.sh
@@ -6,10 +6,11 @@
 OPENSSL=/usr/bin/openssl
 CONFIG="${1%.*}"
 PASSPHRASE=passwd.${CONFIG}
+CA={{ ctlabs_ca.defaults.config.ca }}
 
 if [ -e "${CONFIG}.cnf" ]; then
   ${OPENSSL} rand -base64 -out ${PASSPHRASE} 32
   ${OPENSSL} req -config ${CONFIG}.cnf -new -x509 -verbose -passout file:${PASSPHRASE} -days 365 -newkey rsa -keyout ${CONFIG}.key -out ${CONFIG}.csr
-  ${OPENSSL} x509 -in ${CONFIG}.csr -days 365 -CA ca-test.crt -passin file:passwd.ca-test -CAkey ca-test.key -set_serial 01 -out ${CONFIG}.crt
+  ${OPENSSL} x509 -in ${CONFIG}.csr -days 365 -CA {{ ctlabs_ca.defaults.config.ca.name }}.crt -passin file:passwd.{{ ctlabs_ca.defaults.config.ca.name }} -CAkey {{ ctlabs_ca.defaults.config.ca.name }}.key -set_serial 01 -out ${CONFIG}.crt
   ${OPENSSL} rsa  -in ${CONFIG}.key -passin file:${PASSPHRASE} > ${CONFIG}.prv
 fi
