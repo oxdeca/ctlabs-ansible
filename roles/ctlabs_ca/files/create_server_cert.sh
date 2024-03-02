4f22acbec9db0cdcb9d1cb84d818e28ed88332d8
acedxo <acedxo@oxdeca.com>
2024-03-01 23:09:43 -0500

    fixed server_cert.sh

--- roles/ctlabs_ca/files/create_server_cert.sh
+++ roles/ctlabs_ca/files/create_server_cert.sh
@@ -11,7 +11,7 @@ SERIAL=$(/usr/bin/date +%s%N)
 
 if [ -e "${CONFIG}.cnf" ]; then
   ${OPENSSL} rand -base64 -out ${PASSPHRASE} 32
-  ${OPENSSL} req -config ${CONFIG}.cnf -new -verbose -passout file:${PASSPHRASE} -days 365 -newkey rsa -keyout ${CONFIG}.key -out ${CONFIG}.csr
+  ${OPENSSL} req -config ${CONFIG}.cnf -new -x509 -verbose -passout file:${PASSPHRASE} -days 365 -newkey rsa -keyout ${CONFIG}.key -out ${CONFIG}.csr
   ${OPENSSL} x509 -in ${CONFIG}.csr -days 365 -CA ${CA}.crt -passin file:passwd.${CA} -CAkey ${CA}.key -set_serial ${SERIAL:0:12} -out ${CONFIG}.crt
   ${OPENSSL} rsa  -in ${CONFIG}.key -passin file:${PASSPHRASE} > ${CONFIG}.prv
 fi
