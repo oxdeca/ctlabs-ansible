4a9ec3b36582c1f25b4bf237f39bb3b29d150a63
acedxo <acedxo@oxdeca.com>
2024-03-01 22:51:20 -0500

    update serials; check if dirs exist;

--- roles/ctlabs_ca/files/create_server_cert.sh
+++ roles/ctlabs_ca/files/create_server_cert.sh
@@ -7,10 +7,11 @@ OPENSSL=/usr/bin/openssl
 CONFIG="${1%.*}"
 PASSPHRASE=passwd.${CONFIG}
 CA="${2}"
+SERIAL=$(/usr/bin/date +%s%N)
 
 if [ -e "${CONFIG}.cnf" ]; then
   ${OPENSSL} rand -base64 -out ${PASSPHRASE} 32
-  ${OPENSSL} req -config ${CONFIG}.cnf -new -x509 -verbose -passout file:${PASSPHRASE} -days 365 -newkey rsa -keyout ${CONFIG}.key -out ${CONFIG}.csr
-  ${OPENSSL} x509 -in ${CONFIG}.csr -days 365 -CA ${CA}.crt -passin file:passwd.${CA} -CAkey ${CA}.key -set_serial 01 -out ${CONFIG}.crt
+  ${OPENSSL} req -config ${CONFIG}.cnf -new -verbose -passout file:${PASSPHRASE} -days 365 -newkey rsa -keyout ${CONFIG}.key -out ${CONFIG}.csr
+  ${OPENSSL} x509 -in ${CONFIG}.csr -days 365 -CA ${CA}.crt -passin file:passwd.${CA} -CAkey ${CA}.key -set_serial ${SERIAL:0:12} -out ${CONFIG}.crt
   ${OPENSSL} rsa  -in ${CONFIG}.key -passin file:${PASSPHRASE} > ${CONFIG}.prv
 fi
