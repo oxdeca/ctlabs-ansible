184fce5493a48bbb9cec93debf587daf2b3c8210
oxdeca <acedxo@oxdeca.com>
2024-05-01 03:05:41 -0400

    role netbox update

--- roles/ctlabs_netbox/files/netbox.sh
+++ roles/ctlabs_netbox/files/netbox.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-/opt/netbox/upgrade.sh                                                 && \
-. /opt/netbox/venv/bin/activate                                        && \
-export DJANGO_SUPERUSER_USERNAME="admin"                               && \
-export DJANGO_SUPERUSER_PASSWORD='secret123!'                          && \
-export DJANGO_SUPERUSER_EMAIL="admin@ctlabs.internal"                  && \
-cd /opt/netbox/netbox && python3 ./manage.py createsuperuser --noinput &
+function update() {
+  /opt/netbox/upgrade.sh                                                 && \
+  . /opt/netbox/venv/bin/activate                                        && \
+  export DJANGO_SUPERUSER_USERNAME="admin"                               && \
+  export DJANGO_SUPERUSER_PASSWORD='secret123!'                          && \
+  export DJANGO_SUPERUSER_EMAIL="admin@ctlabs.internal"                  && \
+  cd /opt/netbox/netbox && python3 ./manage.py createsuperuser --noinput &
+  
+  exit 0
+}
 
-exit 0
+function start() {
+  . /opt/netbox/venv/bin/activate && \
+  cd /opt/netbox/netbox           && \
+  nohup python3 ./manage.py runserver 127.0.0.1:8080 --insecure &
+
+  exit 0
+}
+
+case $1 in
+  update)
+    update
+    ;;
+  start)
+    start
+    ;;
+esac
